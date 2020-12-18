80f3b4b876c7ab5b561239ded657db4e35bbc3f8
Dragon-Fish <824399619@qq.com>
2020-12-18 14:44:59 +0800

    Minor fix

--- test.js
+++ test.js
@@ -5,21 +5,22 @@ var words = process.argv
 console.log('=== wordsFilter START ===')
 
 if (!words[2]) {
-  console.log('× No worlds given!')
-  console.log('? Try some sentences like: `yarn test This is bad words`')
+  console.log('┌ × No worlds given!')
+  console.log('├ ? Try this: yarn test This is bad words')
+  console.log('└ ? Or this: yarn test 这句话包含不好的词语')
 } else {
   delete words[0]
   delete words[1]
   words = words.join(' ').trim()
 
   var filter = new Filter({
-    list: ['bad\\s*words?', 'not good'],
+    list: ['bad\\s*words?', '不好'],
     cleanWith: '*',
   })
 
-  console.log('→ input:', words)
-  console.log('→ isBad:', filter.isBad(words))
-  console.log('→ clean:', filter.clean(words))
+  console.log('┌ input:', words)
+  console.log('├ isBad:', filter.isBad(words))
+  console.log('└ clean:', filter.clean(words))
 }
 
-console.log('===  wordsFilter END  ===')
+console.log('=== wordsFilter END  ===')
